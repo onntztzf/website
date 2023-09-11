53c22c393d04320c76d5af6403b15a6a8b994292
2hangpeng <zhangpeng.0304@aliyun.com>
2023-09-11 17:46:56 +0800

    import radix-ui

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,13 +1,14 @@
 import dynamic from "next/dynamic"
 import "./style.css";
+import {Box, Kbd} from "@radix-ui/themes";
 
 const Clock = dynamic(() => import("@/components/clock"), {ssr: false})
 
 export default function Page() {
     return (
-        <div className="index">
+        <Box className="w-screen h-screen flex justify-center items-center">
             <Clock/>
-        </div>
+        </Box>
     );
 }
 
